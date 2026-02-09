8a3d4ef7b3b61ed096a362ef2dbe2997dc242be7
Sherqo <sharqawycs@gmail.com>
2026-02-09 20:29:14 +0200

    Add resume PDF and update links, footer, page header, and 404 page

---
+++ src/lib/links.ts
@@ -0,0 +1,23 @@
+export const links: Record<string, string> = {
+  github: 'https://github.com/sherqo',
+  gh: '/github',
+
+  x: 'https://x.com/sharqawycs',
+  twitter: '/x',
+
+  youtube: 'https://youtube.com/@sharqawycs',
+  yt: '/youtube',
+
+  linkedin: 'https://linkedin.com/in/sharqawycs',
+  li: '/linkedin',
+  in: '/linkedin',
+
+  discord: 'https://discord.gg/sharqawycs',
+  dis: '/discord',
+
+  blog: '/',
+  journal: '/',
+
+  resume: '/resume.pdf',
+  cv: '/resume', // Assuming you have a resume file
+};
