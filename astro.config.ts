b6d834c26db463a8c5a8f0666b9e65557e4197d8
Sharqawy Wael <sherqo@sherqo.me>
2026-09-11 18:09:28 +0300

    feat: add font support and improve layout for Arabic language, update global styles

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import { fileURLToPath, URL } from 'node:url';
 import compress from 'vite-plugin-compression';
 import htmlMinifier from 'vite-plugin-html-minifier';
@@ -20,7 +20,32 @@ export default defineConfig({
   site: 'https://www.sherqo.me',
   integrations: [mdx(), sitemapConfig],
 
-  // Astrso
+  fonts: [
+    {
+      name: 'Inter',
+      cssVariable: '--font-inter',
+      provider: fontProviders.google(),
+      weights: ['400 900'],
+      styles: ['normal'],
+      subsets: ['latin'],
+      formats: ['woff2'],
+      display: 'optional',
+      fallbacks: ['sans-serif'],
+    },
+    {
+      name: 'Noto Sans Arabic',
+      cssVariable: '--font-noto-sans-arabic',
+      provider: fontProviders.google(),
+      weights: ['400 900'],
+      styles: ['normal'],
+      subsets: ['arabic'],
+      formats: ['woff2'],
+      display: 'optional',
+      fallbacks: ['sans-serif'],
+    },
+  ],
+
+  // Astro
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
