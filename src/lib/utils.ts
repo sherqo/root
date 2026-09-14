b8c8fdbb982998f2139d2ccbfca4b285f22854f3
Sharqawy Wael <sherqo@sherqo.me>
2026-09-14 03:43:16 +0300

    chore: refresh homepage introduction copy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const dateFormatter = new Intl.DateTimeFormat('ja-JP', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
